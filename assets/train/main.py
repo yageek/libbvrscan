843866eb6a61a75ac3334339fbda95c6d0c9eb92
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-03-15 12:17:23 +0100

    Basic training

--- assets/train/main.py
+++ assets/train/main.py
@@ -1,5 +1,9 @@
 import numpy
 import scipy.special
+import os
+from os import listdir
+from os.path import isfile, join
+import struct
 
 # neural network class definition
 class neuralNetwork:
@@ -75,14 +79,45 @@ class neuralNetwork:
         
         return final_outputs
 
-# number of input, hidden and output nodes
-input_nodes = 3
-hidden_nodes = 3
-output_nodes = 3
+def load_values(file_path):
+    return numpy.fromfile(file_path, dtype='uint8')
+    
+# Start loading the assets
+assets_dir = os.path.dirname(os.path.realpath(__file__)) + "/../generated"
+assets_files = {}
+input_index = {}
 
-# learning rate is 0.3
+i = 0
+for file in listdir(assets_dir):
+    name, ext = os.path.splitext(file)
+    abs_path = join(assets_dir, file)
+    if isfile(abs_path) and ext == ".ocrb":
+        assets_files[name] = load_values(abs_path)
+        input_index[name] = i
+        i += 1
+
+# number of input, hidden and output nodes
+input_nodes = 1024
+hidden_nodes = 100
+output_nodes = 13
+
+# # learning rate is 0.3
 learning_rate = 0.3
 
 # create instance of neural network
-n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
-a = n.query([1.0, 0.5, -1.5])
+n = neuralNetwork(input_nodes,hidden_nodes,output_nodes,
+learning_rate)
+
+# Train some amount
+epochs = 50
+for e in range(epochs):    
+    for key in assets_files.keys():
+        # Prepare the value
+        inputs = numpy.asfarray(assets_files[key])
+        targets = numpy.zeros(13)
+        targets[input_index[key]] = 1
+        n.train(inputs, targets)
+
+# Test
+print("Output:", input_index)
+print("Test 1", n.query(assets_files["1"]))
