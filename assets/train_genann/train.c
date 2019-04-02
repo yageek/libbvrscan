3963534672ac1e6528edd2c60323bda07832f825
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-04-02 17:20:35 +0200

    Update elements

--- assets/train_genann/train.c
+++ assets/train_genann/train.c
@@ -2,12 +2,28 @@
 #include "genann.h"
 #include <bvrcore.h>
 #include <io.h>
+#include <string.h>
 
 #define INPUTS 1024
 #define OUTPUTS 13
 #define HIDDEN_LAYER_COUNT 1
 #define HIDDEN_LAYER_NODES_COUNT 100
 #define LEARNING_RATE 0.3
+
+
+
+void free_in_data(bvr_mat8_t **asset_data, size_t len) {
+    // Free memory
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        bvr_mat8_t *mat = asset_data[i];
+        if (mat != NULL)
+        {
+            bvr_mat_free(mat);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -32,28 +48,74 @@ int main(int argc, char **argv)
     double current_output[OUTPUTS];
     char input_name[1024];
 
+    // Load the assets dats
     for (i = 0; i < OUTPUTS; i++)
     {
-        // Load the file data
+
         snprintf(input_name, 1024, "%s/%c.png", input_dir, input_index[i]);
         bvr_io_image_source_t *src = bvr_io_load_png(input_name);
-        if (src == NULL) {
+        if (src == NULL)
+        {
             perror("impossible to load image.");
+            free_in_data(asset_data, OUTPUTS);
             exit(-1);
         }
-        asset_data[i] = bvr_io_grayscale_src(src);
+
+        bvr_mat8_t *rgb = bvr_io_rgb_src(src);
+        if(rgb == NULL) {
+            perror("can not load rgb image");
+            bvr_io_image_source_free(src);
+            free_in_data(asset_data, OUTPUTS);
+            exit(-1);
+        }
+        asset_data[i] = bvr_filter_create_grayscale(rgb);
         bvr_io_image_source_free(src);
+        bvr_mat_free(rgb);
     }
 
-    // Free memory
+    // Load and train
+    int epoch = 100;
+    int e;
+    for(e = 0; e < epoch; e++) {
 
-    for (i = 0; i < OUTPUTS; i++)
-    {
-        bvr_mat8_t *mat = asset_data[i];
-        if(mat != NULL) {
-            bvr_mat_free(mat);
+        // For each characters
+        int i;
+        for(i = 0; i < OUTPUTS; i++) {
+
+            // Load the inputs
+            bvr_mat8_t *data = asset_data[i];
+            uint8_t * in_ptr = data->content;
+
+            int j;
+            for(j = 0; j < INPUTS; j++) {
+                double val =  ((double)in_ptr[j]);
+                
+                current_input[j] = val;
+            }
+
+            // Load the outputs
+            memset(current_output, 0, OUTPUTS*sizeof(double));
+            current_input[i] = 1.0;
+
+            // Train the network
+            genann_train(ann, current_input, current_output, LEARNING_RATE);
+
         }
     }
+
+
+    // Test
+    const double *answer = genann_run(ann, current_input);
+    for (i=0; i < OUTPUTS; i++){
+        printf("%f\t", answer[i]);
+    }
+    printf("\n");
+
+
+    // Free memory
+    free_in_data(asset_data, OUTPUTS);
+    genann_free(ann);
+
 
     return 0;
 }
